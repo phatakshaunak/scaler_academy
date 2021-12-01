c845c58589b824689bb0f9ce329e966fa5bff885
Shaunak Phatak <shaunakphatak@Shaunaks-MacBook-Pro.local>
2021-12-01 09:50:34 +0530

    trees problems

---
+++ DSA_Problem_Solving/Trees_2/identical_binary_tree.py
@@ -0,0 +1,89 @@
+'''Q1. Identical Binary Trees
+Solved
+character backgroundcharacter
+Stuck somewhere?
+Ask for help from a TA & get it resolved
+Get help from TA
+Problem Description
+
+Given two binary trees, check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+
+
+
+Problem Constraints
+
+1 <= number of nodes <= 105
+
+
+
+Input Format
+
+First argument is a root node of first tree, A.
+
+Second argument is a root node of second tree, B.
+
+
+
+Output Format
+
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem.
+
+
+
+Example Input
+
+Input 1:
+
+   1       1
+  / \     / \
+ 2   3   2   3
+Input 2:
+
+   1       1
+  / \     / \
+ 2   3   3   3
+
+
+Example Output
+
+Output 1:
+
+ 1
+Output 2:
+
+ 0
+
+
+Example Explanation
+
+Explanation 1:
+
+ Both trees are structurally identical and the nodes have the same value.
+Explanation 2:
+
+ Value of left child of the tree is different.'''
+
+# Definition for a  binary tree node
+# class TreeNode:
+#	def __init__(self, x):
+#		self.val = x
+#		self.left = None
+#		self.right = None
+
+class Solution:
+	# @param A : root node of tree
+	# @param B : root node of tree
+	# @return an integer
+	def isSameTree(self, A, B):
+
+        # When both reach null
+        if not A and not B:
+            return 1
+            
+        # When the trees do not match
+        if not A or not B:
+            return 0
+        
+        return 1 if (A.val == B.val and self.isSameTree(A.left,B.left) and self.isSameTree(A.right, B.right)) else 0
