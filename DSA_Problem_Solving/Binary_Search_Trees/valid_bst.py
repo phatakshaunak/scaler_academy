1bcd46ef40e3a5a6a7d92aa3e5467fc6c36c3172
Shaunak Phatak <shaunakphatak@Shaunaks-MacBook-Pro.local>
2021-12-05 14:39:49 +0530

    bst questions

---
+++ DSA_Problem_Solving/Binary_Search_Trees/valid_bst.py
@@ -0,0 +1,118 @@
+'''Q2. Valid Binary Search Tree
+Solved
+character backgroundcharacter
+Stuck somewhere?
+Ask for help from a TA & get it resolved
+Get help from TA
+Problem Description
+
+Given a binary tree represented by root A.
+
+Assume a BST is defined as follows:
+
+1) The left subtree of a node contains only nodes with keys less than the node's key.
+
+2) The right subtree of a node contains only nodes with keys greater than the node's key.
+
+3) Both the left and right subtrees must also be binary search trees.
+
+
+
+Problem Constraints
+
+1 <= Number of nodes in binary tree <= 100000
+
+0 <= node values <= 10^9
+
+
+
+Input Format
+
+First and only argument is head of the binary tree A.
+
+
+
+Output Format
+
+Return 0 if false and 1 if true.
+
+
+
+Example Input
+
+Input 1:
+
+ 
+   1
+  /  \
+ 2    3
+Input 2:
+
+ 
+  2
+ / \
+1   3
+
+
+Example Output
+
+Output 1:
+
+ 0
+Output 2:
+
+ 1
+
+
+Example Explanation
+
+Explanation 1:
+
+ 2 is not less than 1 but is in left subtree of 1.
+Explanation 2:
+
+Satisfies all conditions.'''
+
+# Definition for a  binary tree node
+# class TreeNode:
+#	def __init__(self, x):
+#		self.val = x
+#		self.left = None
+#		self.right = None
+
+class Solution:
+	# @param A : root node of tree
+	# @return an integer
+	def isValidBST(self, A):
+
+        is_valid = [1]
+
+        self.helper(is_valid, A)
+
+        return is_valid[0]
+    
+    def helper(self, is_valid, root):
+
+        if not root:
+            return info(float('inf'), float('-inf'))
+
+        if is_valid[0]:
+
+            left = self.helper(is_valid, root.left)
+            right = self.helper(is_valid, root.right)
+
+            # Check if not valid (assuming unique elements in a binary tree)
+            if root.val <= left.max_ or root.val >= right.min_:
+                is_valid[0] = 0
+            
+            return info(min(root.val, left.min_), max(root.val, right.max_))
+        
+        else:
+            # Return anything, it won't be checked
+            return info(-1,-1)
+
+
+class info:
+    def __init__(self, min_, max_):
+        self.min_ = min_
+        self.max_ = max_
