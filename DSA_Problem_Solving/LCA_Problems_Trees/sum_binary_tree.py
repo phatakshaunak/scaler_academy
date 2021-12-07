e4a7caa092656a3898c1062f55fb29fba43f1020
Shaunak Phatak <shaunakphatak@Shaunaks-MacBook-Pro.local>
2021-12-07 20:03:32 +0530

    tree questions

--- DSA_Problem_Solving/LCA_Problems_Trees/sum_binary_tree.py
+++ DSA_Problem_Solving/LCA_Problems_Trees/sum_binary_tree.py
@@ -78,27 +78,6 @@ Explanation 2:
 
 # Definition for a  binary tree node
 # class TreeNode:
-#    def __init__(self, x):
-#        self.val = x
-#        self.left = None
-#        self.right = None
-
-# Definition for a  binary tree node
-# class TreeNode:
-#    def __init__(self, x):
-#        self.val = x
-#        self.left = None
-#        self.right = None
-
-# Definition for a  binary tree node
-# class TreeNode:
-#	def __init__(self, x):
-#		self.val = x
-#		self.left = None
-#		self.right = None
-
-# Definition for a  binary tree node
-# class TreeNode:
 #	def __init__(self, x):
 #		self.val = x
 #		self.left = None
