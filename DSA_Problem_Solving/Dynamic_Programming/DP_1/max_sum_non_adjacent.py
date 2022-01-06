2b50f3dffa8f62ab1ab2c744dfb8ff493155d032
Shaunak Phatak <shaunakphatak@Shaunaks-MacBook-Pro.local>
2022-01-06 18:12:09 +0530

    DP

---
+++ DSA_Problem_Solving/Dynamic_Programming/DP_1/max_sum_non_adjacent.py
@@ -0,0 +1,113 @@
+'''Q5. Max Sum Without Adjacent Elements
+Solved
+character backgroundcharacter
+Stuck somewhere?
+Ask for help from a TA & get it resolved
+Get help from TA
+Problem Description
+
+Given a 2 x N grid of integer, A, choose numbers such that the sum of the numbers is maximum and no two chosen numbers are adjacent horizontally, vertically or diagonally, and return it.
+
+Note: You can choose more than 2 numbers.
+
+
+
+Problem Constraints
+
+1 <= N <= 20000
+1 <= A[i] <= 2000
+
+
+
+Input Format
+
+The first and the only argument of input contains a 2d matrix, A.
+
+
+
+Output Format
+
+Return an integer, representing the maximum possible sum.
+
+
+
+Example Input
+
+Input 1:
+
+ A = [   
+        [1]
+        [2]    
+     ]
+Input 2:
+
+ A = [   
+        [1, 2, 3, 4]
+        [2, 3, 4, 5]    
+     ]
+
+
+Example Output
+
+Output 1:
+
+ 2
+Output 2:
+
+ 8
+
+
+Example Explanation
+
+Explanation 1:
+
+ We will choose 2.
+Explanation 2:
+
+ We will choose 3 and 5.'''
+
+class Solution:
+    # @param A : list of list of integers
+    # @return an integer
+    def adjacent(self, A):
+
+        '''
+        For top and bottom positions, either select that element or not.
+        If element is selected, the max sum at A[i][n] will use max(A[i][n] + A[i-2][0], A[i][n] + A[i-2][1]). Do for top and bottom rows, and update answer with max value
+        If not selected, ans will be taken from A[i-1][n]
+        Extension of the house robber problem
+        To store answers, use a dp array of size 2 X N
+        If i - 1 or i - 2 < 0 choose the element A[i][n] as the answer
+        '''
+
+        n = len(A[0])
+
+        dp = [[0 for i in range(n)] for i in range(2)]
+        ans = float('-inf')
+        for i in range(n):
+
+            # Iterate rows
+            for j in range(2):
+
+                if i == 0:
+                    # print(j, i)
+                    dp[j][i] = max(A[0][i], A[1][i])
+                
+                elif i == 1:
+                    dp[j][i] = max(A[0][i], A[1][i], A[0][i-1], A[1][i-1])
+                    
+                else:
+                    # Select element at A[j][i] and dp[j][i-2]
+                    a1 = A[j][i] + dp[0][i-2]
+                    a2 = A[j][i] + dp[1][i-2]
+                    # Don't select element at A[j][i]
+                    a3 = dp[0][i-1]
+                    a4 = dp[1][i-1]
+
+                    # Update with maximum value
+                    dp[j][i] = max(a1, a2, a3, a4)
+                    
+                ans = max(ans, dp[j][i])
+            # print(dp, 'storage array')
+        
+        return ans
---
+++ DSA_Problem_Solving/Dynamic_Programming/DP_2/lis_nlogn.py
@@ -0,0 +1,62 @@
+'''Q5. Length of LIS
+Attempted
+character backgroundcharacter
+Stuck somewhere?
+Ask for help from a TA & get it resolved
+Get help from TA
+Problem Description
+
+You are given an array A. You need to find the length of the Longest Increasing Subsequence in the array.
+
+In other words, you need to find a subsequence of array A in which the elements are in sorted order, (strictly increasing) and as long as possible.
+
+
+
+Problem Constraints
+
+1 ≤ length(A), A[i] ≤ 105
+
+
+
+Input Format
+
+The first and only argument of the input is the array A.
+
+
+
+Output Format
+
+Output a single integer, the length of the longest increasing subsequence in array A.
+
+
+
+Example Input
+
+Input 1:
+
+A: [2, 1, 4, 3]
+Input 2:
+
+A: [5, 6, 3, 7, 9]
+
+
+Example Output
+
+Output 1:
+
+2
+Output 2:
+
+4
+
+
+Example Explanation
+
+Explanation 1:
+
+ [2, 4] and [1, 3] are the longest increasing sequences of size 2. 
+Explanation 2:
+
+The longest increasing subsequence we can get is [5, 6, 7, 9] of size 4.'''
+
+ 
