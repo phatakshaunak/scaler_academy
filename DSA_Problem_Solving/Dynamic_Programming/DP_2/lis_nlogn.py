c861bbf46b051c9a0ce7c45edebb53678d58767c
Shaunak Phatak <shaunakphatak@Shaunaks-MacBook-Pro.local>
2022-01-07 12:56:10 +0530

    DP

--- DSA_Problem_Solving/Dynamic_Programming/DP_2/lis_nlogn.py
+++ DSA_Problem_Solving/Dynamic_Programming/DP_2/lis_nlogn.py
@@ -59,4 +59,64 @@ Explanation 2:
 
 The longest increasing subsequence we can get is [5, 6, 7, 9] of size 4.'''
 
- 
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def findLIS(self, A):
+        
+        # 1 + max(all j = 0 to j- 1 where A[j] < A[i])
+
+        # dp = [0] * len(A)
+        # dp[0] = 1
+
+        # for i in range(1, len(A)):
+            
+        #     # ans = float('-inf')
+        #     ans = 1
+            
+        #     for j in range(i):
+        #         if A[j] < A[i]:
+        #             ans = max(ans, 1 + dp[j])
+            
+        #     dp[i] = ans
+        
+        # return max(dp)
+
+        store = [A[0]]
+
+        n = len(A)
+
+#         lis = 1
+        for i in range(1, n):
+
+            idx = self.upper_bound(store, 0, len(store) - 1, A[i])
+            
+            # print('upper bound', idx, A[i], store)
+            if idx == len(store):
+                store.append(A[i])
+            
+            else:
+                store[idx] = A[i]
+        
+        # print(store, idx)
+        return len(store)
+    
+    # Upper bound should be the index where key should be inserted. If key is present in the array, return leftmost index for key, else return the index of the smallest larger element
+    # If no upper bound, return length of the array
+    def upper_bound(self, arr, s, e, key):
+        
+        ans = len(arr)
+
+        while s <= e:
+            mid = s + (e - s) // 2
+
+            if arr[mid] >= key:
+                ans = mid
+                e = mid - 1
+            
+            else:
+                s = mid + 1
+        
+        return ans
+    
+    # TC O(N^2), SC O(N)
