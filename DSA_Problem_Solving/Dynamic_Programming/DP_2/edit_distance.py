5104ee0742aa08b7d7e30f6ff9661ebcb4b898f0
Shaunak Phatak <shaunakphatak@Shaunaks-MacBook-Pro.local>
2022-01-08 12:18:40 +0530

    DP

--- DSA_Problem_Solving/Dynamic_Programming/DP_2/edit_distance.py
+++ DSA_Problem_Solving/Dynamic_Programming/DP_2/edit_distance.py
@@ -112,15 +112,24 @@ class Solution:
         
         dp = [[0] * (n2 + 1) for i in range(n1 + 1)]
         
-        for i in range(1, n1 + 1):
-            dp[i][0] = i
+        # for i in range(1, n1 + 1):
+        #     dp[i][0] = i
         
-        for j in range(1, n2 + 1):
-            dp[0][j] = j 
+        # for j in range(1, n2 + 1):
+        #     dp[0][j] = j 
 
         for i in range(n1):
                 
             for j in range(n2):
+
+                dp[i + 1][0] = i + 1
+                dp[0][j + 1] = j + 1
+
+                # if i != 0:
+                #     dp[i][0] = i
+                
+                # if j != 0:
+                #     dp[0][j] = j
                 
                 if s1[i] == s2[j]:
                     dp[i + 1][j + 1] = dp[i][j]
