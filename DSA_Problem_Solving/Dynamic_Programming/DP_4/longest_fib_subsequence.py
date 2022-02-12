106ba88015f17d0e607ef114f2ff524eec3db7a9
Shaunak Phatak <shaunakphatak@Shaunaks-MacBook-Pro.local>
2022-02-12 10:09:58 +0530

    DP Updates

--- DSA_Problem_Solving/Dynamic_Programming/DP_4/longest_fib_subsequence.py
+++ DSA_Problem_Solving/Dynamic_Programming/DP_4/longest_fib_subsequence.py
@@ -72,7 +72,7 @@ Explanation 2:
  The longest subsequence that is fibonacci-like: [1, 11, 12], [3, 11, 14] or [7, 11, 18].
  The length will be 3.'''
 
-class Solution:
+ class Solution:
     # @param A : list of integers
     # @return an integer
     def solve(self, A):
@@ -80,15 +80,21 @@ class Solution:
         n = len(A)
 
         dp = [[0 for i in range(n)] for j in range(n)]
+        
+        # Similar to the definition of a fibonacci sequence, dp[i][j] represents the count of elements in a fibonacci sequence with the last two elements as A[i] and A[j]
 
+        # Values on and below the diagonal are not valid, initialize the remaining cells with 2 as that can be the minimum sequence length
+
+        # Recurrence Relation: if A[i] + A[j] == A[k], dp[j][k] = 1 + dp[i][j]. Get the maximum of all dp[j][k]
         for i in range(n):
             for j in range(i + 1, n):
                 dp[i][j] = 2
         
-        ans = float('-inf')
+        ans = 2
 
         for i in range(2, n):
-
+            
+            # Two sum approach (Using two pointers as array is sorted), otherwise use a map
             l, r = 0, i - 1
 
             while l < r:
@@ -104,6 +110,7 @@ class Solution:
                 else:
                     dp[r][i] = 1 + dp[l][r]
                     ans = max(ans, dp[r][i])
+                    
                     l += 1
                     r -= 1
         
